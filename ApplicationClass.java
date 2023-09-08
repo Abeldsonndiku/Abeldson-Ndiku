198c6d563d201b865add85e79bb5002ee34ba26d
Abeldson Ndiku <135463074+Abeldsonndiku@users.noreply.github.com>
2023-09-08 06:21:22 +0300

    Add files via upload

---
+++ ApplicationClass.java
@@ -0,0 +1,10 @@
+package com.codetho.photocollage;
+
+import androidx.multidex.MultiDexApplication;
+
+public class ApplicationClass extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
